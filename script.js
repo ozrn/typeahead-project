0e776bf1e1cdf7844d76910ddac1de49fab27a08
Kevser Ozeren <99321065+ozrn@users.noreply.github.com>
2022-08-14 16:39:19 +0300

    Add async fn to push data into array

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+const cities = [];
+const getCities = async () => {
+  const res = await fetch(endpoint);
+  const data = await res.json();
+  return cities.push(...data);
+}
+getCities();
